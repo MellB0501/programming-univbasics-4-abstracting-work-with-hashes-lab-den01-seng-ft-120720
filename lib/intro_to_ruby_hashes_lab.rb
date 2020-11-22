adf4eb410d072304c3d0b749303b843a95ef741d
Mellissa Basham <mellbasham@gmail.com>
2020-11-22 01:59:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -2,10 +2,12 @@ def my_hash_creator()
   { :key "value"}
 end
 
-def read_from_hash
+def read_from_hash(hash, key)
+  read_from_hash = {hash: key}
 end
 
-def update_counting_hash()
+def update_counting_hash(hash, key)
+  
   if 
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
